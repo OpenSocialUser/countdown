46760865d7bc8168646c9be4ff8d6312e3c47c3d
OpenSocialUser <opensocialuser2015@gmail.com>
2016-11-29 12:02:34 +0300

    SJR-39 fixed setting selectedDate on changing month/year

--- countdown_logic.js
+++ countdown_logic.js
@@ -30,156 +30,152 @@ function toObject(str) {
 function updateCountdown() {
 	var state = wave.getState();
 
-    var digits = "";
-    var targetTime = "";
-    var displayCircles = true;
+  var digits = "";
+  var targetTime = "";
+  var displayCircles = true;
 
-    var readMoreLink = $("#read_more_field").val();
+  var readMoreLink = $("#read_more_field").val();
 
-    var digitsRadio = $("input[type='radio'][name='digits']:checked");
-    if (digitsRadio.length > 0) {
-        digits = digitsRadio.val();
-    } else {
-        digits = "all";
-    }
+  var digitsRadio = $("input[type='radio'][name='digits']:checked");
+  if (digitsRadio.length > 0) {
+    digits = digitsRadio.val();
+  } else {
+    digits = "all";
+  }
 
-    targetTime = selectedDate;
+  targetTime = selectedDate;
 
-    var circlesCheckbox = $("input[type='checkbox'][name='circles']:checked");
-    if (circlesCheckbox.length > 0) {
-        displayCircles = true;
-    } else {
-        displayCircles = false;
-    }
+  var circlesCheckbox = $("input[type='checkbox'][name='circles']:checked");
+  if (circlesCheckbox.length > 0) {
+    displayCircles = true;
+  } else {
+    displayCircles = false;
+  }
 
-    circlesColor = $("#picker").val();
-    if (circlesColor == null || circlesColor == undefined || circlesColor == "") {
-        circlesColor == "40484F";
-    }
+  circlesColor = $("#picker").val();
+  if (circlesColor == null || circlesColor == undefined || circlesColor == "") {
+    circlesColor == "40484F";
+  }
 
-    if (targetTime != null && targetTime != "") {
-        state.submitDelta({'digits' : digits});
-        state.submitDelta({'target_time' : targetTime});
-        state.submitDelta({'display_circles' : displayCircles});
-        state.submitDelta({'circles_color' : circlesColor});
-        state.submitDelta({'read_more_link' : readMoreLink});
+  if (targetTime != null && targetTime != "") {
+    state.submitDelta({'digits' : digits});
+    state.submitDelta({'target_time' : targetTime});
+    state.submitDelta({'display_circles' : displayCircles});
+    state.submitDelta({'circles_color' : circlesColor});
+    state.submitDelta({'read_more_link' : readMoreLink});
 
-        drawCountdown(digits, targetTime, displayCircles, circlesColor, readMoreLink);
-    } else {
-        renderEditPage();
-    }
+    drawCountdown(digits, targetTime, displayCircles, circlesColor, readMoreLink);
+  } else {
+    renderEditPage();
+  }
 }
 
 function drawCountdown(digits, targetTime, displayCircles, circlesColor, readMoreLink) {
-    var html = "";
-    var htmlHeader = "";
-    var htmlFooter = "";
+  var html = "";
+  var htmlHeader = "";
+  var htmlFooter = "";
 
-    var showAllDigits = true;
+  var showAllDigits = true;
 
-    if (digits != null && digits == "days") {
-        showAllDigits = false;
+  if (digits != null && digits == "days") {
+    showAllDigits = false;
+  } else {
+    showAllDigits = true;
+  }
+
+  if (showAllDigits) {
+    html += "<div id='countdown' style='width: 90%;'></div>";
+  } else {
+    var gadgetWidth = $("#countdown_gadget").width()
+    if (gadgetWidth < 300) {
+      html += "<div id='countdown' style='width: 90%;'></div>";
+    } else if (gadgetWidth < 350) {
+      html += "<div id='countdown' style='width: 70%;'></div>";
+    } else if (gadgetWidth < 410) {
+      html += "<div id='countdown' style='width: 58%;'></div>";
+    } else if (gadgetWidth < 500) {
+      html += "<div id='countdown' style='width: 49%;'></div>";
+    } else if (gadgetWidth < 600) {
+      html += "<div id='countdown' style='width: 43%;'></div>";
     } else {
-        showAllDigits = true;
+      html += "<div id='countdown' style='width: 28%;'></div>";
     }
+  }
 
+
+  if (readMoreLink != null && readMoreLink != "") {
     if (showAllDigits) {
-        html += "<div id='countdown' style='width: 90%;'></div>";
+      html += "<div id='read_more'>";
     } else {
-        var gadgetWidth = $("#countdown_gadget").width()
-        if (gadgetWidth < 300) {
-            html += "<div id='countdown' style='width: 90%;'></div>";
-        } else if (gadgetWidth < 350) {
-            html += "<div id='countdown' style='width: 70%;'></div>";
-        } else if (gadgetWidth < 410) {
-            html += "<div id='countdown' style='width: 58%;'></div>";
-        } else if (gadgetWidth < 500) {
-            html += "<div id='countdown' style='width: 49%;'></div>";
-        } else if (gadgetWidth < 600) {
-            html += "<div id='countdown' style='width: 43%;'></div>";
-        } else {
-            html += "<div id='countdown' style='width: 28%;'></div>";
-        }
+      html += "<div id='read_more' style='position: relative; bottom: 20px;'>";
     }
+    html += "<a id='read_more_link' target='_blank' href='" + readMoreLink + "'>Read More</a>"
+    html += "</div>";
+  }
 
+  if (isOwner) {
+    htmlFooter += "<div id='editButtonIcon' onclick='renderEditPage()''></div>";
+  }
 
-    if (readMoreLink != null && readMoreLink != "") {
-        if (showAllDigits) {
-            html += "<div id='read_more'>";
-        } else {
-            html += "<div id='read_more' style='position: relative; bottom: 20px;'>";
-        }
-        html += "<a id='read_more_link' target='_blank' href='" + readMoreLink + "'>Read More</a>"
-        html += "</div>";
+  document.getElementById('body').innerHTML = html;
+  document.getElementById('footer').innerHTML = htmlFooter;
+  document.getElementById('header').innerHTML = htmlHeader;
+
+  $("#countdown").data("date", targetTime);
+
+  $("#countdown").TimeCircles({
+    "count_past_zero": false,
+    "animation": "smooth",
+    "bg_width": 0.2,
+    "fg_width": 0.03,
+    "circle_bg_color": "#90989F",
+    "time": {
+      "Days": {
+        "text": "Days",
+        "color": "#" + circlesColor,
+        "show": true
+      },
+      "Hours": {
+        "text": "Hours",
+        "color": "#" + circlesColor,
+        "show": showAllDigits
+      },
+      "Minutes": {
+        "text": "Minutes",
+        "color": "#" + circlesColor,
+        "show": showAllDigits
+      },
+      "Seconds": {
+        "text": "Seconds",
+        "color": "#" + circlesColor,
+        "show": showAllDigits
+      }
     }
+  });
 
-    if (isOwner) {
-        //htmlFooter += "<button id='editButton' onclick='renderEditPage()''>Edit</button>";
-        htmlFooter += "<div id='editButtonIcon' onclick='renderEditPage()''></div>";
-    }
-
-    document.getElementById('body').innerHTML = html;
-    document.getElementById('footer').innerHTML = htmlFooter;
-    document.getElementById('header').innerHTML = htmlHeader;
-
-    $("#countdown").data("date", targetTime);
-
-    $("#countdown").TimeCircles({
-        "count_past_zero": false,
-        "animation": "smooth",
-        "bg_width": 0.2,
-        "fg_width": 0.03,
-        "circle_bg_color": "#90989F",
-        "time": {
-            "Days": {
-                "text": "Days",
-                "color": "#" + circlesColor,
-                "show": true
-            },
-            "Hours": {
-                "text": "Hours",
-                "color": "#" + circlesColor,
-                "show": showAllDigits
-            },
-            "Minutes": {
-                "text": "Minutes",
-                "color": "#" + circlesColor,
-                "show": showAllDigits
-            },
-            "Seconds": {
-                "text": "Seconds",
-                "color": "#" + circlesColor,
-                "show": showAllDigits
-            }
-        }
-
-    });
-
-    if (displayCircles != null && !displayCircles) {
-        var canvas = $("#circlesCanvas");
-        canvas.hide();
-
-        var timeCirclesDiv = $(".time_circles").first();
-
-        timeCirclesDiv.css("height", canvas[0].height + "px");
-    }
+  if (displayCircles != null && !displayCircles) {
+    var canvas = $("#circlesCanvas");
+    canvas.hide();
+    var timeCirclesDiv = $(".time_circles").first();
+    timeCirclesDiv.css("height", canvas[0].height + "px");
+  }
 }
 
 function checkIfOwner() {
-    var userId = null;
-    var ownerId = null;
+  var userId = null;
+  var ownerId = null;
 
-    osapi.people.getViewer().execute(function(data) {
-        userId = data.id;
-        osapi.people.getOwner().execute(function(data) {
-            ownerId = data.id;
-            if (ownerId != null && userId != null && ownerId == userId) {
-                isOwner = true;
-            } else {
-                isOwner = false;
-            }
-        });
+  osapi.people.getViewer().execute(function(data) {
+    userId = data.id;
+    osapi.people.getOwner().execute(function(data) {
+      ownerId = data.id;
+      if (ownerId != null && userId != null && ownerId == userId) {
+          isOwner = true;
+      } else {
+          isOwner = false;
+      }
     });
+  });
 }
 
 function renderEditPage() {
@@ -195,163 +191,140 @@ function renderEditPage() {
 	var htmlHeader = "";
 	var htmlFooter = "";
 
-    html += "<p style='font-size: 14px;'>Choose digits to display:</p>";
+  html += "<p style='font-size: 14px;'>Choose digits to display:</p>";
 
-    if (digits != null && digits == "days") {
-        html += "<input type='radio' name='digits' value='all'>Days with hour/min/sec</input>";
-        html += "</br>";
-        html += "<input type='radio' name='digits' value='days' checked='true'>Days only</input>";
-    } else {
-        html += "<input type='radio' name='digits' value='all' checked='true'>Days with hour/min/sec</input>";
-        html += "</br>";
-        html += "<input type='radio' name='digits' value='days'>Days only</input>";
-    }
-
+  if (digits != null && digits == "days") {
+    html += "<input type='radio' name='digits' value='all'>Days with hour/min/sec</input>";
     html += "</br>";
-
-    html += "<p style='font-size: 14px;'>Countdown style:</p>";
-
-    if (displayCircles != null && displayCircles == false) {
-        html += "<input type='checkbox' name='circles' value='true'>Display circles</input>";
-    } else {
-        html += "<input type='checkbox' name='circles' value='true' checked='true'>Display circles</input>";
-    }
-
+    html += "<input type='radio' name='digits' value='days' checked='true'>Days only</input>";
+  } else {
+    html += "<input type='radio' name='digits' value='all' checked='true'>Days with hour/min/sec</input>";
     html += "</br>";
+    html += "<input type='radio' name='digits' value='days'>Days only</input>";
+  }
 
-    html += "<p style='font-size: 14px;'>Pick circle color:</p>";
+  html += "</br>";
+  html += "<p style='font-size: 14px;'>Countdown style:</p>";
 
-    if (circlesColor != null && circlesColor != "") {
-        html += "<input id='picker' class='color' value='" + circlesColor + "'/>";
-    } else {
-        html += "<input id='picker' class='color' value='40484F'/>";
-    }
+  if (displayCircles != null && displayCircles == false) {
+    html += "<input type='checkbox' name='circles' value='true'>Display circles</input>";
+  } else {
+    html += "<input type='checkbox' name='circles' value='true' checked='true'>Display circles</input>";
+  }
 
-    html += "</br>";
+  html += "</br>";
+  html += "<p style='font-size: 14px;'>Pick circle color:</p>";
 
-    html += "<p style='font-size: 14px;'>Enter date for the Final Countdown:</p>";
+  if (circlesColor != null && circlesColor != "") {
+    html += "<input id='picker' class='color' value='" + circlesColor + "'/>";
+  } else {
+    html += "<input id='picker' class='color' value='40484F'/>";
+  }
 
-    if (targetTime != null && targetTime != "") {
-        html += "<input id='target_date_picker' type='text' value='" + targetTime + "'/>";
-        selectedDate = targetTime;
-    } else {
-        selectedDate = currentDate();
-        html += "<input id='target_date_picker' type='text' value='" + selectedDate + "'/>";
-    }
+  html += "</br>";
+  html += "<p style='font-size: 14px;'>Enter date for the Final Countdown:</p>";
 
-    html += "</br>";
+  if (targetTime != null && targetTime != "") {
+    html += "<input id='target_date_picker' type='text' value='" + targetTime + "'/>";
+    selectedDate = targetTime;
+  } else {
+    selectedDate = currentDate();
+    html += "<input id='target_date_picker' type='text' value='" + selectedDate + "'/>";
+  }
 
-    html += "<p style='font-size: 14px;'>Enter URL for 'Read More' link (if left empty the link won't be shown):</p>";
+  html += "</br>";
+  html += "<p style='font-size: 14px;'>Enter URL for 'Read More' link (if left empty the link won't be shown):</p>";
 
-    if (readMoreLink != null && readMoreLink != "") {
-        html += "<input id='read_more_field' type='text' value='" + readMoreLink + "'/>";
-        selectedDate = targetTime;
-    } else {
-        html += "<input id='read_more_field' type='text'/>";
-    }
+  if (readMoreLink != null && readMoreLink != "") {
+    html += "<input id='read_more_field' type='text' value='" + readMoreLink + "'/>";
+    selectedDate = targetTime;
+  } else {
+    html += "<input id='read_more_field' type='text'/>";
+  }
 
-    html += "</br>";
-    html += "</br>";
+  html += "</br>";
+  html += "</br>";
 
-    html += "<button id='saveButton' onclick='updateCountdown()''>Save</button>";
-    html += "<button id='cancelButton' onclick='renderCountdown()''>Cancel</button>";
+  html += "<button id='saveButton' onclick='updateCountdown()''>Save</button>";
+  html += "<button id='cancelButton' onclick='renderCountdown()''>Cancel</button>";
 
-    html += "<p>";
-    html += "Please report issues to IT direct component ";
-    html += "<a target='_blank' href='https://itdirect.wdf.sap.corp/sap(bD1lbiZjPTAwMSZkPW1pbg==)/bc/bsp/sap/crm_ui_start/default.htm?saprole=ZITSERVREQU&crm-object-type=AIC_OB_INCIDENT&crm-object-action=D&PROCESS_TYPE=ZINE&CAT_ID=IMAS_JAM'>'IMAS_Jam'</a>";
-    html += ", feedback to ";
-    html += "<a target='_blank' href='https://jam4.sapjam.com/groups/about_page/3B960zLBubCXJjPjDT59T5'>SAP Jam Support Center</a>";
-    html += ".";
-    html += "</p>";
+  html += "<p>";
+  html += "Please report issues to IT direct component ";
+  html += "<a target='_blank' href='https://itdirect.wdf.sap.corp/sap(bD1lbiZjPTAwMSZkPW1pbg==)/bc/bsp/sap/crm_ui_start/default.htm?saprole=ZITSERVREQU&crm-object-type=AIC_OB_INCIDENT&crm-object-action=D&PROCESS_TYPE=ZINE&CAT_ID=IMAS_JAM'>'IMAS_Jam'</a>";
+  html += ", feedback to ";
+  html += "<a target='_blank' href='https://jam4.sapjam.com/groups/about_page/3B960zLBubCXJjPjDT59T5'>SAP Jam Support Center</a>";
+  html += ".";
+  html += "</p>";
 
-    htmlHeader += "<h3>Settings:</h3>";
+  htmlHeader += "<h3>Settings:</h3>";
 
-    document.getElementById('body').innerHTML = html;
-    document.getElementById('footer').innerHTML = htmlFooter;
-    document.getElementById('header').innerHTML = htmlHeader;
+  document.getElementById('body').innerHTML = html;
+  document.getElementById('footer').innerHTML = htmlFooter;
+  document.getElementById('header').innerHTML = htmlHeader;
 
-    $("#target_date_picker").datetimepicker({
-        inline:true,
-        minDate: '-1970/01/01',
-        onChangeDateTime:function(dp, $input){
-            selectedDate = $input.val();
-        },
-        onChangeMonth:function(ct, $input){
-          updateDateTimeToSelected($input);
-        },
-        onChangeYear:function(ct, $input){
-          updateDateTimeToSelected($input);
-        }
-    });
+  $("#target_date_picker").datetimepicker({
+    inline:true,
+    minDate: '-1970/01/01',
+    onChangeDateTime:function(dp, $input){ selectedDate = $input.val(); },
+    onChangeMonth:function(ct, $input){ updateDateTimeToSelected(ct); },
+    onChangeYear:function(ct, $input){ updateDateTimeToSelected(ct); }
+  });
 
-    var colorPicker = new jscolor.color(document.getElementById('picker'), {});
-    colorPicker.fromString($("#picker").val());
+  var colorPicker = new jscolor.color(document.getElementById('picker'), {});
+  colorPicker.fromString($("#picker").val());
 
-    window.onload = gadgets.window.adjustHeight(300);
+  window.onload = gadgets.window.adjustHeight(300);
 }
 
 function renderCountdown() {
-    if (!wave.getState()) {
-        return;
-    }
-    var state = wave.getState();
+  if (!wave.getState()) {
+    return;
+  }
+  var state = wave.getState();
 
-    var targetTime = state.get('target_time');
-    var digits = state.get('digits');
-    var circlesColor = state.get('circles_color');
-    var displayCircles = state.get('display_circles');
-    var readMoreLink = state.get('read_more_link');
+  var targetTime = state.get('target_time');
+  var digits = state.get('digits');
+  var circlesColor = state.get('circles_color');
+  var displayCircles = state.get('display_circles');
+  var readMoreLink = state.get('read_more_link');
 
-    checkIfOwner();
+  checkIfOwner();
 
-    if (targetTime != null && digits != null) {
-    	drawCountdown(digits, targetTime, displayCircles, circlesColor, readMoreLink);
+  if (targetTime != null && digits != null) {
+  	drawCountdown(digits, targetTime, displayCircles, circlesColor, readMoreLink);
+  } else {
+    if (isOwner) {
+     renderEditPage();
     } else {
+      setTimeout(function(){
         if (isOwner) {
-    	   renderEditPage();
-        } else {
-            setTimeout(function(){
-                if (isOwner) {
-                   renderEditPage();
-                }
-            }, 2000);
+          renderEditPage();
         }
+      }, 2000);
     }
+  }
 
-    window.onload = gadgets.window.adjustHeight();
-
-    /*gadgets.window.adjustHeight();
-    setTimeout(function(){
-        gadgets.window.adjustHeight();
-    }, 1500);*/
+  window.onload = gadgets.window.adjustHeight();
 }
 
 function currentDate() {
-  var date = new Date;
-  var hour = date.getHours() + 1;
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var year = date.getFullYear()
-  return formatDate(year, month, day, hour);
+  var d = new Date;
+  return formatDate(d.getFullYear(),d.getMonth()+1,d.getDate(),d.getHours()+1);
 }
 
-function formatDate(year, month, day, hour) {
-  return year+'/'+month+'/'+day+' '+hour+':00'
+function formatDate(y, m, d, h) {
+  return y+'/'+m+'/'+d+' '+h+':00'
 }
 
-function updateDateTimeToSelected(input) {
-  var d = $('td.xdsoft_date.xdsoft_current').data();
-  var t = $('div.xdsoft_time.xdsoft_current').data();
-  input.val(formatDate(d.year, d.month, d.date, t.hour));
-  selectedDate = input.val();
+function updateDateTimeToSelected(ct) {
+  selectedDate = formatDate(ct.getFullYear(),ct.getMonth()+1,ct.getDate(),ct.getHours());
+  input.val(selectedDate);
 }
 
 function init() {
-    if (wave && wave.isInWaveContainer()) {
-        wave.setStateCallback(renderCountdown);
-
-        // wave.setParticipantCallback(renderCountdown);
-    }
+  if (wave && wave.isInWaveContainer()) {
+    wave.setStateCallback(renderCountdown);
+  }
 }
 
 gadgets.util.registerOnLoadHandler(init);
